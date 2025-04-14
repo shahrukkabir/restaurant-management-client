d0c063260dc399cf9a86055114cb8c6cfa9bba87
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-04-14 13:33:26 +0600

    load menu for diff items

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import CommonCover from '../../Shared/CommonCover';
-import img from '../../../../src/assets/menu/banner3.jpg'
 import useMenu from '../../../hooks/useMenu';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 import MenuCategory from '../MenuCategory/MenuCategory';
+import img from '../../../../src/assets/menu/banner3.jpg'
+import img3 from '../../../../src/assets/menu/dessert-bg.jpeg'
+import img4 from '../../../../src/assets/menu/pizza-bg.jpg'
+import img5 from '../../../../src/assets/menu/salad-bg.jpg'
+import img6 from '../../../../src/assets/menu/soup-bg.jpg'
 
 const Menu = () => {
     const [data] = useMenu();
@@ -19,10 +23,23 @@ const Menu = () => {
             <Helmet>
                 <title>Restaurant | Menu</title>
             </Helmet>
+            {/* main cover */}
             <CommonCover title={"OUR MENU"} image={img} description={"Would you like to try a dish?"}></CommonCover>
+            {/* offered title */}
             <SectionTitle heading={"TODAY'S OFFER"} subHeading={"---Don't miss---"}>
             </SectionTitle>
+            {/* offered menu items */}
             <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items */}
+            <MenuCategory items={dessert} title={"DESSERTS"} img={img3} description={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."} ></MenuCategory>
+            {/* dessert menu items */}
+            <MenuCategory items={pizza} title={"PIZZA"} img={img4} description={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."} ></MenuCategory>
+            {/* salads menu items */}
+            <MenuCategory items={salad} title={"SALADS"} img={img5} description={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."} ></MenuCategory>
+            {/* salads menu items */}
+            <MenuCategory items={soup} title={"SOUPS"} img={img6} description={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."} ></MenuCategory>
+            
+            
         </div>
     );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MenuItem from '../../Shared/MenuItem';
+import CommonCover from '../../Shared/CommonCover';
 
-const MenuCategory = ({items}) => {
+const MenuCategory = ({ items, title, description, img }) => {
     return (
         <div>
+            {title && <CommonCover title={title} image={img} description={description}></CommonCover>}
             <div className='grid md:grid-cols-2 gap-y-8 gap-x-16 max-w-6xl mx-auto'>
                 {
                     items.map(item => <MenuItem item={item} key={item._id}></MenuItem>)
