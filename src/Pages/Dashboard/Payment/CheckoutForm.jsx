58652b00226ef31f516f51c97ab3490e555fce56
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-08-20 19:17:28 +0600

    payment saved in db

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -68,13 +68,27 @@ const CheckoutForm = () => {
             }
         })
         if (confirmError) {
-            console.log('confirm error')
+            console.log('confirm error')    
         }
         else {
             console.log('payment intent', paymentIntent)
             if (paymentIntent.status === 'succeeded') {
                 console.log('transaction id', paymentIntent.id);
                 setTransactionId(paymentIntent.id);
+
+                // now save the payment in the database
+                const payment = {
+                    email: user.email,
+                    price: totalPrice,
+                    transactionId: paymentIntent.id,
+                    date: new Date(), // utc date convert -> using moment js  
+                    cartIds: cart.map(item => item._id),
+                    menuItemIds: cart.map(item => item.menuId),
+                    status: 'pending'
+                }
+
+                const res = await axiosSecure.post('/payments', payment);
+                console.log('payment saved', res.data);
             }
         }
     };
